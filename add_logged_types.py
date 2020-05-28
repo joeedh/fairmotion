2f572b915b6447e1804cfc6f7eab9e6612ff3447
Joseph Eagar <joeedh@gmail.com>
2020-05-27 21:06:27 -0700

    update

--- add_logged_types.py
+++ add_logged_types.py
@@ -6,8 +6,11 @@ for f in js_sources.sources:
     if "path.ux" in f: continue
     if not f.endswith(".js"): continue
 
-    command = "python tools\extjs\js_cc.py -gtl -at --type-file electron_build/mytypes.json " + f
+    command = "c:/Python38/python.exe tools/extjs_cc/js_cc.py -gtl -at --type-file electron_build/mytypes.json " + f + " " + f
 
-    print(command)
+    print(f)
     print(os.system(command), "<---")
-    break
+
+    #break
+    sys.stdout.flush()
+    sys.stderr.flush()

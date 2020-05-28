956f54b4ccf482f45fb26c30dfd137af5a9a78a4
Joseph Eagar <joeedh@gmail.com>
2020-05-27 21:01:34 -0700

    commit these files

--- add_logged_types.py
+++ add_logged_types.py
@@ -4,6 +4,10 @@ import js_sources
 files = []
 for f in js_sources.sources:
     if "path.ux" in f: continue
-    if not f.endsWith(".js"): continue
+    if not f.endswith(".js"): continue
 
-    
+    command = "python tools\extjs\js_cc.py -gtl -at --type-file electron_build/mytypes.json " + f
+
+    print(command)
+    print(os.system(command), "<---")
+    break

76bf3c8485ab788e46f6041c35a4e81115b11e8e
Joseph Eagar <joeedh@gmail.com>
2020-06-01 17:50:41 -0700

    added a script to publish gh-pages

---
+++ publish_gh_pages.py
@@ -0,0 +1,27 @@
+import os, sys, os.path, subprocess
+
+def getRepoURL():
+  p = subprocess.run(["git", "remote", "get-url", "origin"], capture_output=True)
+  repo_url = str(p.stdout, "utf8").strip()
+  return repo_url
+
+if not os.path.exists("site"):
+    print("checking out gh-pages into ./site")
+    url = getRepoURL()
+    
+    os.system("git clone " + url + " site")
+    path = os.getcwd()
+    
+    os.chdir("./site")
+    os.system("git checkout gh-pages")
+    os.system("git pull")
+    
+    os.chdir(path);
+  
+path = os.getcwd()
+os.chdir("./site")
+os.system("unzip -o ../dist/html5app.zip")
+os.system("git add *")
+os.system("git commit -a -m \"update gh-pages\"")
+os.system("git push")
+os.chdir(path)
